70851385da172c2918a1e3caca2b134b0d144c88
PyTomSss <151681123+PyTomSss@users.noreply.github.com>
2024-08-06 14:21:31 +0100

    Update - Interacting Particle

--- IPLA_Exp_Functions.py
+++ IPLA_Exp_Functions.py
@@ -108,4 +108,84 @@ def post_params(theta, sigma_y, centers_prior, covariances_prior, weights_prior,
 
     return centers_posteriori, covariances_posteriori, weights_posteriori
 
+
+def PGD(nb_particles, nb_iter, step_size, centers_prior, covariances_prior, weights_prior, theta_0, sigma_y, y_obs,
+        coef_particle = 1, plot = False, plot_true_theta = None) : 
+    """
+    This function executes the Particle Gradient Descent in the context of our experiment. Given :
+    - The number of particles
+    - The number of iterations
+    - The step size
+    - Parameters of the prior distribution
+    - The observed data point "y"
+    - The initializing theta_0
+    """
+
+    theta_t = theta_0
+
+    dx = theta_0.shape[0]
+
+    sample = sample_prior(nb_particles, centers_prior, covariances_prior, weights_prior)
+
+    time_SDE = 0
+
+    theta_traj = np.zeros((nb_iter, dx))
+
+    for i in tqdm(range(nb_iter)) : 
+
+        #We don't need to compute the parameters of the posteriori distribution given the updated theta because we use another formula
+        #centers_post, covariances_post, weights_post = post_params(theta_t, sigma_y, centers_prior, covariances_prior, weights_prior)
+
+        time_SDE += step_size
+
+        ## on prend le gradient selon x de la posterior actualisée avec theta_t et qui est aussi une mixture Gaussienne
+        grad = ((1/sigma_y**2) * theta_t[:, np.newaxis] * (y_obs - np.dot(theta_t, sample.T))).T 
+
+        grad += grad_multimodal_opti(sample, weights_prior, centers_prior, covariances_prior) 
+
+        grad_update = step_size * grad * coef_particle
+        
+        #Noise
+        noise =  np.sqrt(2 * step_size) * np.random.randn(nb_particles, dx)
+
+        sample += grad_update + noise #Warning sign
+
+        #MAJ THETA we need a fct that compute gradient of the potential wrt to theta
+
+        grad_theta = grad_theta_GM(sample, theta_t, y_obs, sigma_y) #renvoie un vecteur avec tous les gradients
+
+        grad_theta_update = np.sum(grad_theta, axis = 0) ## ON MULTIPLIE CHAQUE PARTICLE A SON PAS SPECIFIQUE
+
+        theta_t = theta_t - (step_size / nb_particles) * grad_theta_update 
+
+        theta_traj[i] = theta_t
+
+    centers_post, covariances_post, weights_post = post_params(plot_true_theta, sigma_y, centers_prior, covariances_prior, weights_prior, y_obs)
+
+    if plot :
+
+        generate_multimodal(centers_post, covariances_post, weights_post, sample)
+
+        plt.plot(np.arange(nb_iter), theta_traj)
+        plt.show()
+
+    return theta_traj
+
+
+def marginal_likelihood_obs(theta, x_samples, y, sigma_y):
+    """
+    This function computes the log-likelihood of a parameter theta in the context of our experiment, given the observed data point "y"
+    """
+
+    likelihoods = []
+
+    for x in x_samples:
+
+        mean = np.dot(theta, x)
+
+        ##LOG-LIKELIHOOD
+        likelihood = multivariate_normal.logpdf(y, mean=mean, cov=sigma_y**2)
+        likelihoods.append(likelihood)
+        
+    return np.mean(likelihoods)
     
